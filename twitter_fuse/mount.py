73a172b959e9db5be97aa6050236dc67e072ae7e
Delyan Raychev <delyanr@gmail.com>
2016-03-13 20:07:45 -0700

    Functional again

--- twitter_fuse/mount.py
+++ twitter_fuse/mount.py
@@ -2,39 +2,30 @@ from __future__ import with_statement
 
 import errno
 import os
+import time
 
 from . import twitter
+from .logger import logger
 
 from fuse import FUSE, FuseOSError, Operations
 
-FILE_ATTR_KEYS = (
-    'st_atime', 'st_ctime', 'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid')
-STAT_KEYS = (
-    'f_bavail', 'f_bfree', 'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files',
-    'f_flag', 'f_frsize', 'f_namemax')
+GID = os.getgid()
+UID = os.getuid()
 
 
 class TwitterClient(Operations):
     def __init__(self):
-        self.followers = twitter.get_friends()
+        self.followers, self.errors = twitter.get_friends()
         self.user_tweets = {}
+        self.access_seqnum = 0
 
     def access(self, path, mode):
-        print '--- access ---   path={}'.format(path)
-
-        errors = twitter.get_rate_limit_status().get('errors', {})
-        if errors:
-            for error in errors:
-                print '>'*25, error.get('message')
-            return
-
+        self.access_seqnum += 1
+        # logger.info('[%s]: access:   path=%s, mode=%s', self.access_seqnum, path, mode)
         if path == '/':
             return
 
         screen_name, tweet_id = parse_path(path)
-
-        print 'screen_name -> ', screen_name
-        print 'tweet_id -> ', tweet_id
         if screen_name not in self.followers:
             raise FuseOSError(errno.EACCES)
 
@@ -51,23 +42,35 @@ class TwitterClient(Operations):
 
     def getattr(self, path, fh=None):
         screen_name, tweet_id = parse_path(path)
+        now = int(time.time())
         attr = dict(
-            st_uid=os.getuid(), st_gid=os.getuid(),
-            st_atime=0, st_ctime=0, st_mtime=1457168070,
+            st_uid=UID, st_gid=GID,
+            st_atime=0, st_ctime=0, st_mtime=now,
             st_nlink=0,
             st_mode=16877,
             st_size=68)
-        if tweet_id in self.user_tweets.get(screen_name, {}):
-            # TODO: get the date and make it the file's
-            _, tweet = self.user_tweets[screen_name].get(tweet_id, (None, 0))
-            attr.update(
-                dict(
-                    st_mode=33188,
-                    st_size=len(bytearray(tweet)) if tweet else 0))
+
+        is_file = tweet_id in self.user_tweets.get(screen_name, {}) or 'error' in path
+
+        if is_file:
+            if 'error' in path:
+                errors_str = '\n'.join(self.errors if self.errors else [])
+                attr.update(dict(st_size=len(errors_str)))
+            else:
+                # TODO: get the date and make it the file's
+                timestamp, tweet = self.user_tweets[screen_name].get(tweet_id, (None, 0))
+                attr.update(dict(st_mtime=timestamp,
+                                 st_size=len(bytearray(tweet)) if tweet else 0))
+            attr.update(dict(st_mode=33188))
+        logger.info('[mount] getattr: path=%s, fh=%s, attr=%s', path, fh, attr)
         return attr
 
     def readdir(self, path, fh):
+        logger.info('[mount] readdir: path=%s, fh=%s, followers=%s, self.user_tweets=%s',
+                    path, fh, self.followers, self.user_tweets)
         dirs = ['.', '..']
+        if self.errors:
+            dirs.append('errors')
         if path == '/':
             dirs.extend(self.followers)
         else:
@@ -77,6 +80,7 @@ class TwitterClient(Operations):
             yield r
 
     def statfs(self, path):
+        # logger.info('[mount] statfs: path=%s', path)
         return dict(
             f_bsize=1048576,
             f_bavail=3348393,
@@ -90,9 +94,13 @@ class TwitterClient(Operations):
             f_flag=0)
 
     def open(self, path, flags):
+        logger.info('[mount] open: path=%s, flags=%s', path, flags)
         return 12
 
     def read(self, path, length, offset, fh):
+        logger.info('[mount] read: path=%s, length=%s, offset=%s, fh=%s', path, length, offset, fh)
+        if 'error' in path:
+            return '\n'.join(self.errors if self.errors else [])
         screen_name, tweet_id = parse_path(path)
         _, tweet = self.user_tweets.get(screen_name, {}).get(tweet_id, (0, ''))
         return ''.join([chr(x) for x in tweet[offset: offset + length]])
--- twitter_fuse/twitter.py
+++ twitter_fuse/twitter.py
@@ -1,67 +1,98 @@
+import datetime
 import requests
+import time
 
 from .oauth import get_oauth
 from .logger import logger
 
 
+PRE = 'https://api.twitter.com/1.1'
+MAX_TOTAL_FRIENDS = 5
+MAX_FRIENDS_PER_REQUEST = 20
+
+
+def timestamp(string):
+    '''Convert string date to timestamp'''
+    return int(time.mktime(
+        datetime.datetime.strptime(
+            string.replace('+0000 ', ''),
+            '%a %b %d %H:%M:%S %Y'
+        ).utctimetuple()))
+
+
 def get_friends():
-    max_total_friends = 5
-    max_friends_per_request = 5
     screen_name = get_settings().get('screen_name')
     cursor = -1
     friends = set()
+    sequence = 0
     while True:
-        url = 'https://api.twitter.com/1.1/friends/ids.json?screen_name={}'.format(screen_name)
-        url += '&count={}'.format(max_friends_per_request)
+        sequence += 1
+        url = '{}/friends/list.json?screen_name={}'.format(PRE, screen_name)
+        url += '&count={}'.format(MAX_FRIENDS_PER_REQUEST)
         url += '&cursor={}'.format(cursor)
-        r = requests.get(url, auth=get_oauth())
-        response = r.json()
-        new_cursor = response.get('next_cursor')
-        friends.update(set(response.get('ids', [])))
-        if new_cursor and len(friends) < max_total_friends:
-            cursor = new_cursor
-        else:
+        logger.info('[%s] Fetching @%s\'s friends: %s', sequence, screen_name, url)
+        response = requests.get(url, auth=get_oauth()).json()
+        errors = set()
+        if 'errors' in response:
+            for error in response['errors']:
+                errors.add(error['message'])
+                logger.error('[twitter][get_friends] Error: %s', error['message'])
             break
-    return friends
+        logger.info('[twitter] get_friends -> %s', response)
+        new_cursor = response.get('next_cursor')
+        friends.update(set(user['screen_name'] for user in response.get('users', [])))
+        if not new_cursor or len(friends) >= MAX_TOTAL_FRIENDS or new_cursor == cursor:
+            break
+        cursor = new_cursor
+    logger.info('[twitter] Friends: %s', friends)
+    return friends, errors
 
 
 def get_settings():
-    logger.info('Getting your settings.')
-    url = 'https://api.twitter.com/1.1/account/settings.json'
+    logger.info('[twitter] Getting your settings.')
+    url = '{}/account/settings.json'.format(PRE)
     return requests.get(url, auth=get_oauth()).json()
 
 
 def get_tweets(screen_name):
     last_id = None
-    user_tweets = []
+    user_tweets = {}
     max_tweets_per_request = 50
     max_total_tweets = 50
+    sequence = 0
     while True:
-        logger.info('Getting tweets for @%s', screen_name)
-        url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+        sequence += 1
+        logger.info('[twitter][%s] Getting tweets for @%s', sequence, screen_name)
+        url = '{}/statuses/user_timeline.json'.format(PRE)
         url += '?screen_name={}'.format(screen_name)
         url += '&count={}'.format(max_tweets_per_request)
         if last_id:
             url += '&max_id={}'.format(last_id)
-        logger.info('Fetching %s', url)
+        logger.info('[twitter] Fetching %s', url)
 
         response = requests.get(url, auth=get_oauth())
 
         if not response:
-            logger.info('DONE getting tweets for @%s', screen_name)
+            logger.info('[twitter] DONE: getting tweets for @%s', screen_name)
             break
-        new_tweets = [(t['id_str'], t['created_at'], bytearray(t['text'], 'utf-8'))
+        new_tweets = [(t['id_str'], timestamp(t['created_at']), bytearray(t['text'], 'utf-8'))
                       for t in response.json()]
+        if not new_tweets:
+            logger.info('[twitter] DONE: now new tweets for @%s', screen_name)
+            break
         new_last_id = new_tweets[-1][0]
         if new_last_id == last_id or len(user_tweets.get(screen_name, [])) >= max_total_tweets:
-            logger.info('DONE getting tweets for @%s', screen_name)
+            logger.info('[twitter] DONE: getting tweets for @%s', screen_name)
             break
         else:
             last_id = new_last_id
-            user_tweets.extend(new_tweets)
-    return user_tweets
+            user_tweets.setdefault(screen_name, [])
+            user_tweets[screen_name].extend(new_tweets)
+    return user_tweets[screen_name]
 
 
 def get_rate_limit_status():
-    url = 'https://api.twitter.com/1.1/application/rate_limit_status.json'
-    return requests.get(url, auth=get_oauth()).json()
+    url = '{}/application/rate_limit_status.json'.format(PRE)
+    rate_limit_status = requests.get(url, auth=get_oauth()).json()
+    logger.info('[twitter] Get rate limit status: %s', rate_limit_status)
+    return rate_limit_status
